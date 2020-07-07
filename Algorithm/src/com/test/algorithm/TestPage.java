481cad4adb5b4ce63c2efa0c7f7a59b023110de0
shin <ssh9308@naver.com>
2020-07-08 00:34:00 +0900

    알고리즘

---
+++ Algorithm/src/com/test/algorithm/TestPage.java
@@ -0,0 +1,15 @@
+package com.test.algorithm;
+
+public class TestPage {
+	public static void main(String[] args) {
+		
+		
+		for (int i = 0; i < 100 ; i++) {
+			System.out.println(Math.random()*10);
+		}
+		
+		
+		
+	}
+
+}
