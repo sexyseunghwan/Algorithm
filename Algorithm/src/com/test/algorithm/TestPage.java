20a40c537d85d01c12ef6c79d99951bd46163226
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2020-09-06 16:48:16 +0900

    수정

--- Algorithm/src/com/test/algorithm/TestPage.java
+++ Algorithm/src/com/test/algorithm/TestPage.java
@@ -1,36 +1,21 @@
 package com.test.algorithm;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class TestPage {
 	public static void main(String[] args) throws Exception{
 		
-		
-		//List<Integer> list = new ArrayList<Integer>();
-		
-		int[] list = {10,22,3,43,5};
-
-		
-		
-		Arrays.sort(list);
-		
-		
-		
-		for (int i = 0; i < list.length; i++) {
-			System.out.println(list[i]);
-		}
-		
+	
 	}
-
 }
+
+
 
 
 
