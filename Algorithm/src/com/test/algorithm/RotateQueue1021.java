9fff83f6d8ff605234683d1e1af14c02eabb51e3
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2020-09-12 01:26:24 +0900

    수정

--- Algorithm/src/com/test/algorithm/RotateQueue1021.java
+++ Algorithm/src/com/test/algorithm/RotateQueue1021.java
@@ -5,81 +5,56 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class RotateQueue1021 {
+/*
+	  
+문제
+지민이는 N개의 원소를 포함하고 있는 양방향 순환 큐를 가지고 있다. 지민이는 이 큐에서 몇 개의 원소를 뽑아내려고 한다.
+
+지민이는 이 큐에서 다음과 같은 3가지 연산을 수행할 수 있다.
+
+첫 번째 원소를 뽑아낸다. 이 연산을 수행하면, 원래 큐의 원소가 a1, ..., ak이었던 것이 a2, ..., ak와 같이 된다.
+왼쪽으로 한 칸 이동시킨다. 이 연산을 수행하면, a1, ..., ak가 a2, ..., ak, a1이 된다.
+오른쪽으로 한 칸 이동시킨다. 이 연산을 수행하면, a1, ..., ak가 ak, a1, ..., ak-1이 된다.
+큐에 처음에 포함되어 있던 수 N이 주어진다. 그리고 지민이가 뽑아내려고 하는 원소의 위치가 주어진다. 
+(이 위치는 가장 처음 큐에서의 위치이다.) 이때, 그 원소를 주어진 순서대로 뽑아내는데 드는 2번, 3번 연산의 최솟값을 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 큐의 크기 N과 뽑아내려고 하는 수의 개수 M이 주어진다. N은 50보다 작거나 같은 자연수이고, M은 N보다 작거나 같은 자연수이다. 
+둘째 줄에는 지민이가 뽑아내려고 하는 수의 위치가 순서대로 주어진다. 
+위치는 1보다 크거나 같고, N보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄에 문제의 정답을 출력한다.
+
+예제 입력 1 
+10 3
+1 2 3
+예제 출력 1 
+0
+
+예제 입력 2 
+10 3
+2 9 5
+예제 출력 2 
+8
+
+예제 입력 3 
+32 6
+27 16 30 11 6 23
+예제 출력 3 
+59
+
+예제 입력 4 
+10 10
+1 6 3 2 7 9 8 4 10 5
+예제 출력 4 
+14	  
+	 
+	 * */
 	public static void main(String[] args) throws Exception{
-		
-		//m1();
-		m2();
-		
-		
-		
-		
-		
-		
-	}
-
-	private static void m2() throws Exception {
-		
-		//2. 큐를 이용한 방법
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		String[] input1 = br.readLine().split(" ");
-		
-		int queueSize = Integer.parseInt(input1[0]);//큐의크기
-		int pickNum = Integer.parseInt(input1[1]);//뽑으려 하는 수의 개수
-		
-		//비교리스트.
-		List<Integer> compList = new ArrayList<Integer>();
-		
-		for (int i = 0; i < queueSize; i++) {
-			
-		}
-		
-		//큐 생성. -> 1 ~ qeueSize 만큼의 숫자를 넣어줄 큐를 만들어주는것이다.(자연수의 큐)
-		Queue<Integer> queue = new LinkedList<Integer>();
-		
-		//큐에 자연수를 넣어줄 것이다 차례로
-		for (int i = 1; i <= queueSize; i++) {
-			queue.offer(i);
-		}
-		
-		//뽑고싶은 숫자.
-		int[] pickList = new int[pickNum];
-		
-		String[] input2 = br.readLine().split(" ");
-		
-		//뽑고싶은 숫자를 리스트에 넣어준다.
-		for (int i = 0; i < input2.length; i++) {
-			pickList[i] = Integer.parseInt(input2[i]);//문자를 숫자형으로 변환해서 정수 리스트에 집어넣는다.
-		}
-		
-		int answerCount = 0;//2,3번째 연산을 몇회 시행하였는지.
-		int pickIndex = 0;//pickList 를 카운트 해줄것이다.
-		
-		
-		while(pickIndex < pickList.length) {
-			
-			if (pickList[pickIndex] == queue.peek()) {
-				//내가 찾던 숫자인 경우
-				queue.poll();//해당 숫자를 꺼낸다.
-			} else {
-				//내가 찾던 숫자가 아닌경우.
-				
-			}
-			
-		}
-		
-	}
-
-	private static void m1() throws Exception {
-		//1. 리스트를 이용한 방법.
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -90,12 +65,11 @@ public class RotateQueue1021 {
 		int pickNum = Integer.parseInt(input1[1]);//뽑으려 하는 수의 개수
 		
 		//큐의 역할을 수행하는 리스트 생성
-		int[] queue = new int[queueSize];
+		List<Integer> queue = new ArrayList<Integer>();
 		
 		for (int i = 1; i <= queueSize; i++) {
-			queue[i-1] = i;	
+			queue.add(i);
 		}
-		
 		
 		//뽑고싶은 숫자.
 		int[] pickList = new int[pickNum];
@@ -108,18 +82,34 @@ public class RotateQueue1021 {
 		}
 		
 		int answerCount = 0;//2,3번째 연산을 몇회 시행하였는지. -> 최종답 산출물.
-		int pickIndex = 0;//pickList 를 카운트 해줄것이다.
 		
-//		while(pickIndex < pickList.length) {
-//			pickList[index]
-//		}
+		for (int i = 0; i < pickNum; i++) {
+			boolean flag = false;
+			int num = pickList[i];//지금 비교할 숫자
+			
+			while(!flag) {
+				if(queue.get(0) == num) {
+					queue.remove(0);
+					flag = true;
+				} else {
+					if (queue.indexOf(num) <= queue.size() / 2) {
+						//왼쪽으로 쉬프트.
+						queue.add(queue.size()-1,queue.remove(0));
+					} else {
+						//오른쪽으로 쉬프트.
+						queue.add(0,queue.remove(queue.size()-1));
+					}
+					
+					answerCount++;
+				}
+ 				
+			}
+		}//for
 		
-		for (int i = 0; i < pickList.length; i++) {
-			
-			
-			
-		}
+		bw.write(answerCount + "");
+		bw.close();
 		
 		
 	}
+		
 }
--- Algorithm/src/com/test/algorithm/TestPage.java
+++ Algorithm/src/com/test/algorithm/TestPage.java
@@ -14,17 +14,45 @@ import java.util.StringTokenizer;
 public class TestPage {
 	public static void main(String[] args) throws Exception{
 		
-		List<Integer> compList = new ArrayList<Integer>();//비교리스트.
+//		Queue<Integer> queue1 = new LinkedList<Integer>();
+//		Queue<Integer> queue2 = new LinkedList<Integer>();
+//		
+//		for (int i = 1; i <= 10; i++) {
+//			queue1.offer(i);
+//		}
+//		
+//		queue2 = queue1;
+//		
+//		System.out.println(queue2);
 		
-		for (int i = 1; i <= 10; i++) {
-			compList.add(i);
+		//queue1.poll();//queue1이랑 연동이 되네 ㅋㅋㅋㅋㅋㅋ;;
+		//queue1.poll();//queue1이랑 연동이 되네 ㅋㅋㅋㅋㅋㅋ;;
+		
+		//System.out.println(queue2);
+		
+		
+		List<Integer> list = new ArrayList<Integer>();
+		
+		for (int i = 1; i <= 11; i++) {
+			list.add(i);
 		}
 		
-		compList.remove(0);		
+		//list.add(list.remove(0));
+		//System.out.println(list);
 		
-		System.out.println(compList);
+		//list.set(0, 22);
+		//우측 쉬프트 알고리즘 적용해야되는거...
+		
+		System.out.println(list);
 		
 		
+		
+		//System.out.println(Math.ceil((list.size() / 2.0)));
+		//System.out.println((list.size() / 2.0));
+		
+		//System.out.println(list);
+		
+		//System.out.println(list.remove(3));//return 을 하는 놈이네?!
 		
 	}
 }
