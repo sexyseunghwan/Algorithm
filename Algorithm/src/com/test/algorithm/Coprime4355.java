721d89bb31b5e6eb13c4a8729c4510e3395dcde9
shin <ssh9308@naver.com>
2020-10-01 23:20:02 +0900

    수정

--- Algorithm/src/com/test/algorithm/Coprime4355.java
+++ Algorithm/src/com/test/algorithm/Coprime4355.java
@@ -7,25 +7,53 @@ import java.io.OutputStreamWriter;
 
 public class Coprime4355 {
 	public static void main(String[] args) throws Exception{
+		
+		//1은 모든수와 서로소이다.
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		//코프라임을 풀어보자.
+		StringBuffer sb = new StringBuffer();
+		
+		
 		while(true) {
 			
 			int input = Integer.parseInt(br.readLine());//입력한 값
+			int count = 1;
+			int flag = 0;
 			
 			if (input != 0) {
 				
+				for (int i = 2; i < input; i++) {
+					
+					for (int j = 2; j <= i; j++) {
+						
+						if (input % j == 0 && i % j == 0) {
+							flag = 1;
+							break;
+						} 
+						
+					}//for2
+					
+					if (flag == 1) {
+						break;
+					} else {
+						count++;
+					}
+					
+				}//for1  수정?!
 				
+				sb.append(count + "\n");
 				
 			} else {
 				//0 을 적으면 멈춰준다.
 				break;
 			}
-			
 		}
 		
+		bw.write(sb + "");
+		br.close();
+		bw.close();
 		
 	}
 
--- Algorithm/src/com/test/algorithm/TestPage.java
+++ Algorithm/src/com/test/algorithm/TestPage.java
@@ -7,7 +7,21 @@ import java.io.OutputStreamWriter;
 
 public class TestPage {
 	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+//		StringBuffer sb = new StringBuffer();
+//		
+//		sb.append("1\n");
+//		sb.append("2\n");
+//		sb.append("3\n");
+//		sb.append("4\n");
+//		
+//		
+//		System.out.println(sb);
+		
+		
+		//달팽이 달팽이
 		
 		
 	}
