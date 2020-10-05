5a2943a8479aff6c450b49ba36742892dc3afb2b
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2020-10-05 23:03:43 +0900

    수정

--- Algorithm/src/com/test/algorithm/TestPage.java
+++ Algorithm/src/com/test/algorithm/TestPage.java
@@ -7,12 +7,50 @@ import java.io.OutputStreamWriter;
 
 public class TestPage {
 	public static void main(String[] args) throws Exception {
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String[] input = br.readLine().split(" ");//입력받기
+		
+		int n = Integer.parseInt(input[0]);//N명의 사람이 존재
+		int k = Integer.parseInt(input[1]);//K번쨰 요세푸스 순열
+		
+		int[] numArr = new int[n+1];//처음 수열 준비 1 ~ 지정한 숫자까지의 array를 만들것이다.
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("<");
+		
+		int count = 0;//카운트 해줄것
+		int realNum = 1;
+		
+		for (int i = 1; i <= n; i++) {
+			
+			count = 0;
+			
+			while (true) {
+				
+				if (numArr[realNum] == 0) count++;
+				
+				if (count == k) {
+					numArr[realNum] = 1;//수열에 1로 지정하면 지워졌다는 뜻이 된다.
+					
+					if (i == n) sb.append(realNum + ">"); 
+					else sb.append(realNum + ", ");
+					
+					break;
+				}
+				
+				realNum++;
+				
+				if (realNum > n) realNum %= n;
+			}//while
+			
+		}//for
 		
 		
-		
-		
+		bw.write(sb + "");
+		br.close();
+		bw.close();
 		
 		
 	}
--- Algorithm/src/com/test/algorithm/TestPage2.java
+++ Algorithm/src/com/test/algorithm/TestPage2.java
@@ -8,67 +8,12 @@ public class TestPage2 {
  
     public static void main(String[] args) throws IOException {
  
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        // reader writer 생성
- 
-        String input = br.readLine();
-        // 라인 읽어서
- 
-        String[] arr = input.split(" ");
- 
-        int count = Integer.parseInt(arr[0]);
-        // 총 갯수와
-        int term = Integer.parseInt(arr[1]);
-        // 텀 세팅
- 
-        Queue<Integer> queue = new LinkedList<Integer>();
-        // 큐 linkedlist로 구현
- 
-        for (int i = 1; i <= count; i++) {
-            queue.offer(i);
-        }
-        // 1부터 count 까지 큐에 추가
- 
-        StringBuilder sb = new StringBuilder("<");
-        // 스트링 빌더 생성
- 
-        while (!queue.isEmpty()) {
-            // queue가 빌때까지
- 
-            for (int j = 1; j <= term; j++) {
-                // term 만큼 반복하는데
- 
-                if (j != term) {
-                    // 마지막이 아닐때는
-                    queue.offer(queue.poll());
-                    // 맨 앞에 있는 걸 맨 뒤로 돌리고
- 
-                } else {
-                    // 마지막 일 때는
-                    sb.append(queue.poll());
-                    // 가장 처음 숫자를 지우면서 sb에 추가
-                    sb.append(", ");
-                    // 쉼표 찍어주고
-                }
-            }
- 
-        }
- 
-        String temp = sb.substring(0, sb.length() - 2);
-        // 쉼표 듸어쓰기 제거
-        sb = new StringBuilder(temp);
- 
-        sb.append(">");
-        // 괄호 닫아주고
- 
-        bw.write(sb.toString());
-        // 출력
-        
-        br.close();
-        bw.flush();
-        bw.close();
-        // reader와 writer를 닫는다
+      
+    	
+    	int[] numArr = new int[10];
+    	
+    	
+    	
  
     }
  
