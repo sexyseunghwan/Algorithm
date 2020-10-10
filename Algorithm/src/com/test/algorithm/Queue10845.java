429ac70ff507c0575fce96da792b33c23c2065c0
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2020-10-11 01:14:23 +0900

    수정

--- Algorithm/src/com/test/algorithm/Queue10845.java
+++ Algorithm/src/com/test/algorithm/Queue10845.java
@@ -66,6 +66,87 @@ public class Queue10845 {
 	
 	public static void main(String[] args) throws Exception{
 		
+		//List 를 쓸때와 안쓸떄 크게 차이가 나지는 않는다.
+		
+		//m1();
+		m2();
+		
+	}
+
+	private static void m2() throws Exception{
+		//두번쨰 방법
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int cycle = Integer.parseInt(br.readLine());//몇번 싸이클 돌건지 정해준다.
+
+		StringBuffer sb = new StringBuffer();//스트링버퍼
+		
+		int popIndex = 0;
+		int size = 0;
+		int index = 0;
+		
+		int[] queue = new int[cycle];
+		
+		for (int i = 0; i < cycle; i++) {
+			
+			String[] input = br.readLine().split(" ");
+			
+			String command = input[0];
+			
+			if (input.length == 2) {
+				//push 를 함
+				queue[index] = Integer.parseInt(input[1]);
+				index++;
+				size++;
+				
+			} else {
+				//push 이외 모두를 뜻함.
+				if(command.equals("pop")) {
+					if (size == 0) {
+						sb.append("-1\n");
+					} else {
+						sb.append(queue[popIndex] + "\n");
+						popIndex++;
+						size--;
+					}
+
+				} else if (command.equals("size")) {
+					
+					sb.append(size + "\n");
+					
+				} else if (command.equals("empty")) {
+					
+					if (size == 0) sb.append("1\n");
+					else sb.append("0\n");
+					
+				} else if (command.equals("front")) {
+					
+					if (size == 0) sb.append("-1\n");
+					else sb.append(queue[popIndex] + "\n");
+					
+				} else {
+					//back 인 경우
+					if (size == 0) sb.append("-1\n");
+					else sb.append(queue[index-1] + "\n");
+					
+				}
+				
+			}
+			
+		}//for
+		
+		bw.write(sb + "");
+		br.close();
+		bw.close();
+		
+		
+	}
+
+	private static void m1() throws Exception{
+		
+		//첫번째 방법
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
--- Algorithm/src/com/test/algorithm/TestPage.java
+++ Algorithm/src/com/test/algorithm/TestPage.java
@@ -1,22 +1,78 @@
 package com.test.algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class TestPage {
+	
 	public static void main(String[] args) throws Exception {
 
-		List<Integer> list = new ArrayList<Integer>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(4);
+		int cycle = Integer.parseInt(br.readLine());//몇번 싸이클 돌건지 정해준다.
 		
-		System.out.println(list.get(0));
-		System.out.println("삭제되는 숫자 : " + list.remove(0));
+		StringBuffer sb = new StringBuffer();//스트링버퍼
 		
-		System.out.println(list.get(0));
+		int popIndex = 0;
+		int size = 0;
+		int index = 0;
+		
+		int[] queue = new int[cycle];
+		
+		for (int i = 0; i < cycle; i++) {
+			
+			String[] input = br.readLine().split(" ");
+			
+			String command = input[0];
+			
+			if (input.length == 2) {
+				//push 를 수행한다.
+				queue[index] = Integer.parseInt(input[1]);
+				index++;
+				size++;
+				
+			} else {
+				//push 이외 모두를 뜻함.
+				if(command.equals("pop")) {
+					
+					if (size == 0) {
+						sb.append("-1\n");
+					} else {
+						sb.append(queue[popIndex] + "\n");
+						popIndex++;
+						size--;
+					}
+
+				} else if (command.equals("size")) {
+					
+					sb.append(size + "\n");
+					
+				} else if (command.equals("empty")) {
+					
+					if (size == 0) sb.append("1\n");
+					else sb.append("0\n");
+					
+				} else if (command.equals("front")) {
+					
+					if (size == 0) sb.append("-1\n");
+					else sb.append(queue[popIndex] + "\n");
+					
+				} else {
+					//back 인 경우
+					if (size == 0) sb.append("-1\n");
+					else sb.append(queue[index-1] + "\n");
+				}
+			}
+			
+		}//for
+		
+		bw.write(sb + "");
+		br.close();
+		bw.close();
+		
 		
 	}
 }
