6ace2436df70ab7e76247da50589c2ad4b9da151
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2020-10-16 22:47:12 +0900

    수정

--- Algorithm/src/com/test/algorithm/TestPage2.java
+++ Algorithm/src/com/test/algorithm/TestPage2.java
@@ -10,15 +10,16 @@ public class TestPage2 {
     public static void main(String[] args) throws IOException {
  
       
-    	 Queue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
-             //컴페얼 메서드도 기억이 안나고 가물가물하냐 ㅆ끼발 
+    	 Queue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() { 
     		 @Override
              public int compare(Integer o1, Integer o2) {
                  if (Math.abs(o1) > Math.abs(o2) || (Math.abs(o1) == Math.abs(o2) && o1 > o2)) return 1;
                  else return -1;
              }
          });
-    
+    	 
+    	 
+    	 
     	
     	
  
