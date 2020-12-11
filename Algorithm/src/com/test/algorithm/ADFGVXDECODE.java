88c02c0a4fe6d0c0d618b32e932523d8aa81c4ce
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2020-12-11 20:38:23 +0900

    2020-12-11

--- Algorithm/src/com/test/algorithm/ADFGVXDECODE.java
+++ Algorithm/src/com/test/algorithm/ADFGVXDECODE.java
@@ -27,6 +27,14 @@ public class ADFGVXDECODE {
 		
 	}
 	
+	static void arrPrint(String[] arr) {
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+	}
+	
 	static void arrPrint(List<String> arr) {
 		
 		System.out.println(arr);
@@ -57,7 +65,7 @@ public class ADFGVXDECODE {
 		Arrays.sort(decKeySplitArr);//정렬 -> s e c u r -> c e r s u 로 변환
 		
 		int stepOneRow = inputList.length / decKey.length();// 2차 암호화때 도출된 매트릭스 행의 개수
-		int stepOneCol = inputList.length;// 2차 암호화때 도출된 매트릭스 열의 개수
+		int stepOneCol = decKey.length();// 2차 암호화때 도출된 매트릭스 열의 개수
 		
 		String[][] stepOneMatrix = new String[stepOneRow][stepOneCol];
 		
@@ -81,29 +89,93 @@ public class ADFGVXDECODE {
 			}
 		}//for
 		
-		arrPrint(stepOneMatrix);
+		arrPrint(stepOneMatrix);//이상 없음
+		
+		System.out.println("=============================");
 		
 		// ============ 암호판 ============
-//		String[][] initialMatrix = new String[7][7];//여기에 암호들이 들어있음.
-//		String[] standard = {"A","D","F","G","V","X"};
-//		String[] innerContents = {"K","Z","W","R","1","F","9","B","6","C","L","5",
-//									"Q","7","J","P","G","X","E","V","Y","3","A","N",
-//									"8","O","D","H","0","2","U","4","I","S","T","M"};
-//		
-//		for (int i = 1; i <= standard.length; i++) {
-//			initialMatrix[0][i] = standard[i-1]; 
-//			initialMatrix[i][0] = standard[i-1]; 
-//		}
-//		
-//		int shift = 0;
-//		
-//		for (int i = 1; i < 7; i++) {
-//			for (int j = 1; j < 7; j++) {
-//				initialMatrix[i][j] = innerContents[shift++];
-//			}
-//		}
+		String[][] initialMatrix = new String[7][7];//여기에 암호들이 들어있음.
+		String[] standard = {"A","D","F","G","V","X"};
+		String[] innerContents = {"K","Z","W","R","1","F","9","B","6","C","L","5",
+									"Q","7","J","P","G","X","E","V","Y","3","A","N",
+									"8","O","D","H","0","2","U","4","I","S","T","M"};
+		
+		for (int i = 1; i <= standard.length; i++) {
+			initialMatrix[0][i] = standard[i-1]; 
+			initialMatrix[i][0] = standard[i-1]; 
+		}
+		
+		int shift = 0;
+		
+		for (int i = 1; i < 7; i++) {
+			for (int j = 1; j < 7; j++) {
+				initialMatrix[i][j] = innerContents[shift++];
+			}
+		}
 		// ============ 암호판 ============
 		
+		
+		String[] finalArr = new String[stepOneRow*stepOneCol];//마지막 결과배열.
+		int finalArrIndex = 0;
+		
+		for (int i = 0; i < stepOneRow; i++) {
+			for (int j = 0; j < stepOneCol; j++) {
+				finalArr[finalArrIndex++] = stepOneMatrix[i][j];
+			}
+		}
+		
+		arrPrint(finalArr);//이상 없음.
+		System.out.println("============");
+		arrPrint(initialMatrix);
+		
+		
+		String answerVal = "";//최종 복호화 답
+		
+		int finalIndex = 0;
+		int swich = 1;
+		String finalRow = "";
+		String finalCol = "";
+		String[] resultVoca = {"A","D","F","G","V","X"};
+		
+		
+		while(!finalArr[finalIndex].equals("P")) {
+			System.out.println("?");
+			swich *= -1;
+			
+			if (swich == 1) {
+				finalCol = finalArr[finalIndex++];
+				int row = 0;
+				int col = 0;
+				
+				//여기서부터 탐색에 들어가야 한다. -> 나중에 업글을 시켜보자. -> 성능 튜닝.
+				for (int i = 0; i < resultVoca.length; i++) {
+					if (finalRow.equals(resultVoca[i])) {
+						row = i+1;
+						break;
+					}
+				}
+				
+				for (int i = 0; i < resultVoca.length; i++) {
+					if (finalCol.equals(resultVoca[i])) {
+						col = i+1;
+						break;
+					}
+				}
+				System.out.println("****");
+				System.out.println(row);
+				System.out.println(col);
+				System.out.println("****");
+				answerVal += initialMatrix[row][col];
+				
+			} else {
+				finalRow = finalArr[finalIndex++];
+				
+			}
+		}//while
+		
+		System.out.println(answerVal);
+		//끝난거지 뭐 
+		System.out.println("ASD");
 		
 	}
 
