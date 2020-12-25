ba3ed6fab4fecc2691dcc53fbdd1543227b04f82
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2020-12-25 22:39:49 +0900

    2020-12-25

---
+++ Algorithm/src/com/test/algorithm/TestTestTest.java
@@ -0,0 +1,40 @@
+package com.test.algorithm;
+
+public class TestTestTest {
+
+	
+	public static void main(String[] args) {
+		
+		String apple = "seq_no	int	4	no";
+		
+		
+		for (int i = 0; i < apple.length(); i++) {
+			
+			
+			System.out.println(apple.substring(i,i+1));
+			
+			System.out.println(apple.substring(i,i+1).matches("^[a-zA-Z0-9]$"));
+			
+			System.out.println("===================");
+		}
+		
+		
+//		if (word.matches("^[a-zA-Z0-9]$") && !word.equals("_")) {
+//			if (voca.equals("")) {
+//				
+//			} else {
+//				inputs[index] = voca;
+//				index++;
+//			}
+//			voca = "";
+//		} else {
+//			voca += line.substring(i,i+1);
+//		}
+//		
+		
+		
+		//System.out.println("	".equals(""));
+		
+	}
+	
+}
---
+++ Qoo10Work/src/com/test/qoo10/Staging.java
@@ -0,0 +1,5 @@
+package com.test.qoo10;
+
+public class Staging {
+
+}
