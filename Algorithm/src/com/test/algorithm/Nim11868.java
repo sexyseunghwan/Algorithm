09b05ec6bdbcd8c1f025a0cc4edafbb15b68efbe
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-03-22 21:49:27 +0900

    2021-03-22

---
+++ Algorithm/src/com/test/algorithm/Nim11868.java
@@ -0,0 +1,27 @@
+package com.test.algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Nim11868 {
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		int cycle = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < cycle; i++) {
+			
+			//이거 쉬운게아닌데? ㅋㅋ
+			
+		}
+		
+		
+	}
+
+}
