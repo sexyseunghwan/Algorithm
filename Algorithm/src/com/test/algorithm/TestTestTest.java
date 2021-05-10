68609e0c7d0dc9956ef2622daa5dc0cdcad25864
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-05-10 23:20:27 +0900

    2021-05-10

--- Algorithm/src/com/test/algorithm/TestTestTest.java
+++ Algorithm/src/com/test/algorithm/TestTestTest.java
@@ -5,39 +5,14 @@ public class TestTestTest {
 	
 	public static void main(String[] args) {
 		
-//		String apple = "seq_no	int	4	no";
-//		
-//		
-//		for (int i = 0; i < apple.length(); i++) {
-//			
-//			
-//			System.out.println(apple.substring(i,i+1));
-//			
-//			System.out.println(apple.substring(i,i+1).matches("^[a-zA-Z0-9]$"));
-//			
-//			System.out.println("===================");
-//		}
+
 		
-		String a = "asd apple index";
+		String a = "hello";
 		
-		System.out.println(a.contains("apple"));
+		System.out.println(a.toCharArray()[0] + 1);
 		
 		
-//		if (word.matches("^[a-zA-Z0-9]$") && !word.equals("_")) {
-//			if (voca.equals("")) {
-//				
-//			} else {
-//				inputs[index] = voca;
-//				index++;
-//			}
-//			voca = "";
-//		} else {
-//			voca += line.substring(i,i+1);
-//		}
-//		
 		
-		
-		//System.out.println("	".equals(""));
 		
 	}
 	
