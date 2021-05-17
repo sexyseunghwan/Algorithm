20bbb5b155f4a250670400a57b980c263ca61a08
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-05-17 22:12:53 +0900

    2021-05-17

--- Algorithm/src/com/test/algorithm/TestPage.java
+++ Algorithm/src/com/test/algorithm/TestPage.java
@@ -8,7 +8,30 @@ import java.util.StringTokenizer;
  
 public class TestPage {
  
-	
+	public static void main(String[] args) {
+		
+
+		LinkedListTest ltt = new LinkedListTest();
+		
+		
+		ltt.addFirst("apple");
+		ltt.addFirst("samsung");
+		ltt.addLast("hwawei");
+		ltt.add(1, "kiss the rain");
+		ltt.add(0, "oh rain");
+		
+		
+		//System.out.println(ltt.node(0));
+		//System.out.println(ltt.node(1));
+		//System.out.println(ltt.node(2));
+		//System.out.println(ltt.node(ltt.getSize()-1));
+		
+		System.out.println(ltt.toString());
+		
+		System.out.println(ltt.indexOf("kiss the rain"));
+		
+		
+	}
 	
 }
 
