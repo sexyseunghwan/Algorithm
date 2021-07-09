bfd12b9c5de727cd624728893ef9013494936377
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-07-09 21:30:08 +0900

    2021-07-09

--- Algorithm/src/com/test/algorithm/TestPage.java
+++ Algorithm/src/com/test/algorithm/TestPage.java
@@ -1,34 +1,18 @@
 package com.test.algorithm;
 
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+
+import java.util.Properties;
 import java.util.StringTokenizer;
+
  
 public class TestPage {
- 
+	  
 	public static void main(String[] args) {
 		
-
-		LinkedListTest ltt = new LinkedListTest();
+		String pn = "01051393792";
 		
-		
-		ltt.addFirst("apple");
-		ltt.addFirst("samsung");
-		ltt.addLast("hwawei");
-		ltt.add(1, "kiss the rain");
-		ltt.add(0, "oh rain");
-		
-		
-		//System.out.println(ltt.node(0));
-		//System.out.println(ltt.node(1));
-		//System.out.println(ltt.node(2));
-		//System.out.println(ltt.node(ltt.getSize()-1));
-		
-		System.out.println(ltt.toString());
-		
-		System.out.println(ltt.indexOf("kiss the rain"));
+		System.out.println(pn.substring(0, 2));
 		
 		
 	}
