d6b0596ff30ccc100cf6be928e2bae748f9066bb
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2021-07-09 21:35:51 +0900

    error

--- Algorithm/src/com/test/algorithm/TestPage.java
+++ Algorithm/src/com/test/algorithm/TestPage.java
@@ -4,12 +4,29 @@ package com.test.algorithm;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.Random;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
  
 public class TestPage {
  
 	
-	
+	public static void main(String[] args) {
+		
+		
+		
+		String email = "ssh9308@naver.com";
+		
+		StringTokenizer stk = new StringTokenizer(email,"@");
+		String userId = stk.nextToken();
+		String userAddress = stk.nextToken();
+		
+		System.out.println(userId);
+		System.out.println(userAddress);
+		
+		
+		
+	}
 }
 
 
